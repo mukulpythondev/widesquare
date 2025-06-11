78ace3011d6469fe0510182bd32d5b843bbfeb37
Mukul Rana <mukulgenious123@gmail.com>
2025-06-11 23:59:24 +0530

    fix the cors

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,8 @@ app.use(cors({
     'http://localhost:5174',
     'http://localhost:5173',
     'http://localhost:5175',
-    'https://widesquare.in'
+    'https://widesquare.in',
+    'https://www.widesquare.in'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'], // Added HEAD
