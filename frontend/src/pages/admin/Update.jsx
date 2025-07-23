0e8fd8a341bf2748995fb5f01d169eb5656386ac
adarsh0133 <adarsh90399@gmail.com>
2025-07-23 15:09:06 +0530

    plot added

--- frontend/src/pages/admin/Update.jsx
+++ frontend/src/pages/admin/Update.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-hot-toast';
 import { backendurl } from "../../config";
 import { X, Upload } from 'lucide-react';
 
-const PROPERTY_TYPES = ['House', 'Apartment', 'Office', 'Villa'];
+const PROPERTY_TYPES = ['House', 'Apartment', 'Office', 'Villa','Plot','Shop','Flat','Farmhouse','Warehouse','Commercial Space','industrial Property'];
 const AVAILABILITY_TYPES = ['rent', 'buy'];
 const AMENITIES = ['Lake View', 'Fireplace', 'Central heating and air conditioning', 'Dock', 'Pool', 'Garage', 'Garden', 'Gym', 'Security system', 'Master bathroom', 'Guest bathroom', 'Home theater', 'Exercise room/gym', 'Covered parking', 'High-speed internet ready'];
 
